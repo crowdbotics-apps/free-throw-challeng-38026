4412c6f84a494a053a70bc57c3899e02f7b60774
Crowdbotics <team@crowdbotics.com>
2022-12-27 17:15:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "free_throw_challeng_38026",
+  url: "https://free_throw_challeng_38026.botics.co",
+  api: "https://free_throw_challeng_38026.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
